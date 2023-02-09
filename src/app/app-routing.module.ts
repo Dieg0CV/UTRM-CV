db1bac03d9246d033510ed83f6271b197342a8c9
Dieg0CV <77285071+Dieg0CV@users.noreply.github.com>
2023-02-09 17:15:47 -0500

    actualizacion inicio 09/02/23

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './modules/inicio/inicio.component';
+
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
